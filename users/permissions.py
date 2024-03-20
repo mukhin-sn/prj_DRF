44b61a4d3a369cd2d5963cf8085cd3e27ac076ad
mukhin-sn <senikomu@gmail.com>
2024-03-21 03:13:33 +0700

    error correction

--- users/permissions.py
+++ users/permissions.py
@@ -8,3 +8,10 @@ class IsModer(BasePermission):
         if request.user.role == Roles.MODERATOR:
             return True
         return False
+
+
+class IsMaster(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        if request.user == obj.master:
+            return True
+        return False
