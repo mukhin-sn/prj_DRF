98cc259b7148e9a4de9dac4767cf61c4c43cfaa7
Sergey Mukhin <senikomu@gmail.com>
2024-03-20 22:30:57 +0700

    error correction

--- users/urls.py
+++ users/urls.py
@@ -3,17 +3,23 @@ from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.views.payments import PaymentListView
-from users.views.users import UserViewSet
+from users.views.users import UserCreateView, UserListView, UserRetrieveView, UserUpdateView, UserDestroyView
+# from users.views.users import UserViewSet
 from users.apps import UsersConfig
 
 app_name = UsersConfig.name
 
-router = DefaultRouter()
-router.register(r'user', UserViewSet)
+# router = DefaultRouter()
+# router.register(r'user', UserViewSet)
 
 urlpatterns = [
     path('payment/', PaymentListView.as_view(), name='payments'),
+    path('create/', UserCreateView.as_view(), name='user_create'),
+    path('list/', UserListView.as_view(), name='user_list'),
+    path('retrieve/<int:pk>/', UserRetrieveView.as_view(), name='user_retrieve'),
+    path('update/<int:pk>/', UserUpdateView.as_view(), name='user_update'),
+    path('destroy/<int:pk>/', UserDestroyView.as_view(), name='user_destroy'),
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 
-    ] + router.urls
+    ]    # + router.urls
--- users/views/users.py
+++ users/views/users.py
@@ -1,9 +1,39 @@
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.viewsets import ModelViewSet
 
 from users.models import User
 from users.serializers.users_serializer import UserSerializer
 
 
-class UserViewSet(ModelViewSet):
+# class UserViewSet(ModelViewSet):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+
+class UserListView(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class UserCreateView(generics.CreateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
+
+class UserRetrieveView(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class UserUpdateView(generics.UpdateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class UserDestroyView(generics.DestroyAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
