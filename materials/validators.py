dd28ac64d209f50c7873de912b8117d49759a5f2
mukhin-sn <senikomu@gmail.com>
2024-04-12 01:53:45 +0700

    updating the video link checking function

--- materials/validators.py
+++ materials/validators.py
@@ -2,6 +2,21 @@ from rest_framework.exceptions import ValidationError
 
 
 def validate_link_to_video(value):
-    if 'youtube.com' not in value:
-        raise ValidationError('Неразрешенная ссылка')
-    return value
+    resolved_link = 'youtube.com'
+    # if 'youtube.com' not in value:
+    #     raise ValidationError('Неразрешенная ссылка')
+
+    # Если адрес содержит разрешенную ссылку (resolved_link),
+    # а также перед и/или после разрешенной комбинации символов в ссылке находятся символы "/",
+    # то адрес разрешен
+    index_start = value.lower().find(resolved_link)
+    index_end = value.lower().rfind(resolved_link)
+    if index_start != -1 and index_start == index_end:
+        if not index_start or value[index_start-1] == "/":
+            if len(value) > index_start+len(resolved_link):
+                if value[index_start+len(resolved_link)] == '/':
+                    return value
+            else:
+                return value
+    raise ValidationError('Неразрешенная ссылка')
+
